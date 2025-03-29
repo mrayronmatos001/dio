189a4534b8879059062ecc117f37ee2b40234de4
mrayronmatos001 <mrayronmatos01@gmail.com>
2025-03-29 17:13:32 -0300

    first attempt at deploying on Railway

--- decola_tech_2025/api-restful/src/main/java/me/dio/DecolaTech2025Application.java
+++ decola_tech_2025/api-restful/src/main/java/me/dio/DecolaTech2025Application.java
@@ -2,12 +2,18 @@ package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DecolaTech2025Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DecolaTech2025Application.class, args);
-	}
+		Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
+		SpringApplication.run(DecolaTech2025Application.class, args);
+
+		System.out.println("PGHOST: " + System.getProperty("PGHOST"));
+	}
+	
 }
