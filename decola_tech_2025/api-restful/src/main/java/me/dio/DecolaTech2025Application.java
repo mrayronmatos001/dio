f8946841452fc0e25f859ca303ccb58bea4a810e
mrayronmatos001 <mrayronmatos01@gmail.com>
2025-04-25 12:16:02 -0300

    list1 and list2 ed finished

--- decola_tech_2025/api-restful/src/main/java/me/dio/DecolaTech2025Application.java
+++ decola_tech_2025/api-restful/src/main/java/me/dio/DecolaTech2025Application.java
@@ -3,7 +3,10 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class DecolaTech2025Application {
 
